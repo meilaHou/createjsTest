61e46bde7c25af7cc6852e9e6f458c34fb41084d
houxl <houxl@git.com>
2016-05-30 14:57:44 +0800

    1.添加筹码使用的功能 2.游戏状态的管理

--- createjstest/src/core/view/panel/BjlSkinPanel.ts
+++ createjstest/src/core/view/panel/BjlSkinPanel.ts
@@ -2,39 +2,82 @@
 /// <include path="../../../../egame/bjlskin.ts" />
 
 module game {
-   export class BjlSKinPanel extends SkinPanel {
+    export class BjlSKinPanel extends SkinPanel {
+        public isInit: boolean = false;
+
        constructor(ss: any) {
            super(ss, "bjlskin_atlas_")
-            this.inits();
+            this.init();
        }
-        private inits() {
-            this.content = new bjllib.bjlskin();
-            this.addChild(this.content);
-            this.addEventListener("tick",this.ontickHandler.bind(this));
-            this.resourceLoader.type = this.ssKey;
-            this.loadSceneRes();
+       public init() {
+           super.init();
+            
+
+            if (this.isInit) {
+                this.initStartStatus();
+            } else {
+                this.resourceLoader.type = this.ssKey;
+                this.loadSceneRes();
+            }
+
        }
-        private choumaBounds: any;
+
+        private choumaBounds:createjs.Container;
         private ontickHandler(evt) {
             if (!this.choumaBounds) return;
+            if (this.choumaBounds.numChildren == 0) return;
             this.choumaBounds.x = this.stage.mouseX;
             this.choumaBounds.y = this.stage.mouseY;
+
+            this.hittestFunc();
+        }
+        private hittestFunc() {
+            for (var i = 1; i < 6; i++) {
+                var tempmc: createjs.MovieClip = this.tablemc["betsItemMc" + i];
+                //检测有问题;
+                //if (tempmc.hitTest(this.choumaBounds.x, this.choumaBounds.y)){
+                //    tempmc.gotoAndStop(3);
+                //} else {
+                //    tempmc.gotoAndStop(1);
+                //};
+
+                if (this.collisionX(this.choumaBounds, tempmc)) {
+                    tempmc.gotoAndStop(3);
+                } else {
+                    tempmc.gotoAndStop(1);
+                };
+            }
+        }
+
+        private collisionX(a, b) {
+            return a.x + a.regX > b.x - b.regX    // a的右边超出了b的左边
+            && a.x - a.regX < b.x + b.regX    // b的右边超出了a的左边
+            && a.y + a.regY > b.y - b.regY    // a的下边超出了b的上边
+            && a.y - a.regY < b.y + b.regY;   // b的下边超出了a的上边
         }
         private loadSceneRes() {
             this.resourceLoader.loadManifest(bjllib.properties.manifest, false);
 
             this.resourceLoader.load();
         }
-        protected handleComplete(evt) {
-            super.handleComplete(evt);
+        protected handleLoadComplete(evt) {
+            super.handleLoadComplete(evt);
+
             console.log("子类响应....");
             this.initStartStatus();
         }
         private tablemc:createjs.MovieClip;
         private initStartStatus() {
+
+            this.content = new bjllib.bjlskin();
+            this.addChild(this.content);
+            this.addEventListener("tick", this.ontickHandler.bind(this));
+            this.choumaBounds = new createjs.Container();
+            this.choumaBounds.mouseEnabled = false;
             this.tablemc = this.content["instance"]; 
             for (var i = 1; i < 6; i++) {
-                this.tablemc["betsItemMc"+i].clearBtn.visible = false;
+                this.tablemc["betsItemMc" + i].clearBtn.visible = false;
+                this.tablemc["betsItemMc" + i].addEventListener("click",this.betItemClickHandler.bind(this));
             }
             this.tablemc["playerPointMc"].visible = false;
             this.tablemc["bankerPointMc"].visible = false;
@@ -51,7 +94,7 @@ module game {
             this.tablemc["opBtnsMc"]["clearBetsBtn"].addEventListener("click", this.clearBetsBtnClickHandler.bind(this));
             this.tablemc["opBtnsMc"]["flyPaiBtn"].addEventListener("click", this.flyPaiBtnClickHandler.bind(this));
             this.tablemc["opBtnsMc"]["reBetsBtn"].addEventListener("click", this.reBetsBtnClickHandler.bind(this));
-
+            this.tablemc["recyclePokerMc"].addChild(new comnpokelib.FeipaiAnim());
 
             //筹码
             var chouma: Array<string> = new Array<string>("10","20","50","100","200");
@@ -60,11 +103,15 @@ module game {
                 this.tablemc["chipsBarMc"]["mc" + i]["txt"].text = chouma[i];
                 this.tablemc["chipsBarMc"]["mc" + i].addEventListener("click", this.choumaClick.bind(this));
             }
-            this.createFapai();
             this.changeState(FAPAI);
+        }
+        private betItemClickHandler(evt){
+            
+            this.changeState(YAZHU);
         }
 
         private createFapai() {
+            this.stage.mouseChildren = true;
             for (var i = 0; i < 4; i++)
             {
                 var fapaimc: createjs.MovieClip = new comnpokelib.FlipPokerMc_0();
@@ -82,86 +129,125 @@ module game {
                 this.tablemc["PokerMc"].addChildAt(fapaimc, i);
             }
         }
-
+        //响应操作
         private kaipai() {
+            this.stage.mouseEnabled = false ;
+            this.stage.mouseChildren = false;
             var pokemc: createjs.MovieClip = this.tablemc["PokerMc"];
             for (var i = 0; i < pokemc.numChildren; i++) {
                 var tempmc: createjs.MovieClip = pokemc.getChildAt(i) as createjs.MovieClip;
                 tempmc.play();
             }
+            //一秒延迟后收牌;
+            this.shoupai();
+            
         }
+        //响应操作
         private shoupai() {
             var pokemc: createjs.MovieClip = this.tablemc["PokerMc"];
             var adsf = new createjs.Shape();
             for (var i = 0; i < pokemc.numChildren; i++) {
                 var tempmc: createjs.MovieClip = pokemc.getChildAt(i) as createjs.MovieClip;
-                
+                tempmc.name = "tempmc"+i;
                 //tempmc.gotoAndStop(1);
                 //var point = new createjs.Point(40, 60);
                 //point = tempmc.globalToLocal(point.x, point.y);
-                createjs.Tween.get(tempmc).wait(1000).call(function (evt,data) {
-                    if (data) data[0].gotoAndStop(1);
-                }, [tempmc],this).to({ x: -470, y: tempmc.y + 40, visible: true }, 1000);
+                createjs.Tween.get(tempmc).wait(1000).call(this.waitCallFunc, [tempmc, pokemc], this).to({ x: -470, y: tempmc.y + 80, scaleX: 0.5, scaleY: 0.5, visible: true }, 800).wait(1).call(this.waitCallFunc2.bind(this));
             }
-
         }
+        private jiesuan() {
+            for (var i = 0; i < this.choumaArr.length; i++) {
+                var chouma: createjs.MovieClip = this.choumaArr[i]; chouma.numChildren
+                createjs.Tween.get(chouma).wait(1000).to({ x: 525, y: 0, visible: false }, 800);
+            }
+            setTimeout(this.changeState.bind(this), 3000, FAPAI);
+        }
+        //第一个传递的是对应的target ,第二个传递的是target的容器;
+        waitCallFunc(evt, data,dddd) {
+            if (evt) {
+                evt.gotoAndStop(1);
+                //var tempfeipai = new comnpokelib.FeiPai();
+                //tempfeipai.x = evt.x;
+                //tempfeipai.y = evt.y;
+                //evt.parent.addChild(tempfeipai);
+                
+            }
+            
+        }
+
+        waitCallFunc2(evt, data, dddd) {
+            this.tablemc["PokerMc"].removeChild(evt.target);
+            if (this.tablemc["PokerMc"].numChildren == 0) {
+                this.changeState(JIESUAN);
+            }
+        }
+        private choumaArr: Array<createjs.MovieClip> = new Array<createjs.MovieClip>();
         private choumaClick(evt) {
             //var obj = evt.target["txt"];
            // var adf = this;
            // var adfadf = this["txt"];
-            //var obj = evt.currentTarget["txt"].text;
-            var obj = evt.currentTarget as createjs.MovieClip;
+            var objtext = evt.currentTarget["txt"].text;
+            //var obj = evt.currentTarget as createjs.MovieClip;
+            var obj = new bjllib.红黄();
+            this.choumaArr.push(obj);
+
+            obj["txt"].text = objtext;
            // alert(sfsafs);
             //var myBounds = utils.ClassUtils.getObjByClass(utils.ClassUtils.getClassName(obj));
             this.removeChild(this.choumaBounds);
-            //this.choumaBounds = obj.clone();//movieclip 不能被克隆;
+            this.choumaBounds.addChild(obj);//movieclip 不能被克隆;
             this.addChild(this.choumaBounds);
-
+            
 
             
-            this.testFunc();
+            //this.testFunc();
         }
 
         private testFunc() {
-            //============clone==============
-            var text = new createjs.Text("Hello World", "20px Arial", "#ff7700");
-            createjs.Tween.get(text).to({ guide: { path: [0, 0, 0, 200, 200, 200, 200, 0, 0, 0] } }, 7000);
-            var shapes = new createjs.Shape();
-            //shapes.graphics.beginFill("0xff00ff");
-            shapes.graphics.moveTo(0, 0).curveTo(0, 200, 200, 200).curveTo(200, 0, 0, 0);
-            shapes.cache(0, 0, 200, 200);
-            text.x = 100;
-            text.y = 50;
-            //text.textBaseline = "alphabetic";
-            var clonetext = text.clone();
-            clonetext.text = "nihao shijie!!!";
-            this.stage.addChild(shapes);
-            this.stage.addChild(text);
-            this.stage.addChild(clonetext);
-            //=========AlphaMaskFilter=======AlphaMapFilter=======
-            var box = new createjs.Shape();
-            //box.graphics.beginLinearGradientFill(["#ff0000", "#0000ff"], [0, 1], 0, 0, 0, 100)
-            box.graphics.beginLinearGradientFill(["#000000", "rgba(0, 0, 0, 0)"], [0, 1], 0, 0, 100, 100)
-            box.graphics.drawRect(0, 0, 100, 100);
-            box.cache(0, 0, 100, 100);
-            var bmp = new createjs.Bitmap("/images/Chrysanthemum.jpg");
-            bmp.filters = [
-               // new createjs.AlphaMapFilter(box.cacheCanvas as HTMLCanvasElement)//第一种遮罩
-                new createjs.AlphaMaskFilter(box.cacheCanvas as HTMLCanvasElement)//第二种遮罩
-            ];
-            bmp.cache(0, 0, 100, 100);
-            this.stage.addChild(bmp);
-            //===============BlurFilter==============
+            //不能添加到舞台;
+            var asdf: createjs.BitmapText = new createjs.BitmapText("你好,bitmaptext");
+            this.addChild(asdf);
+            var myInstance = new bjllib.开牌按钮();
+            this.addChild(myInstance);
+            var helper = new createjs.ButtonHelper(myInstance, "0", "1", "2", false, myInstance, "3");
+
             var shape = new createjs.Shape().set({ x: 100, y: 100 });
             shape.graphics.beginFill("#ff0000").drawCircle(0, 0, 50);
+            var matrix = new createjs.ColorMatrix().adjustHue(180).adjustSaturation(100);
 
-            var blurFilter = new createjs.BlurFilter(50, 5, 1);
-            shape.filters = [blurFilter];
-            var bounds = blurFilter.getBounds();
+            shape.filters = [
+                new createjs.ColorFilter(0.5, 0, 0, 1, 255, 0, 255, 100),
+                new createjs.ColorMatrixFilter(matrix)
+            ];
+            //如果添加了滤镜效果,那么久必须缓存
+            shape.cache(-50, -50, 100, 100);
+            this.addChild(shape);
 
-            shape.cache(-50 + bounds.x, -50 + bounds.y, 100 + bounds.width, 100 + bounds.height);
-            this.stage.addChild(shape);
-            
+            var g = new createjs.Graphics();
+            g.setStrokeStyle(1);
+            g.beginStroke("#000000");
+            g.beginFill("red");
+            g.drawCircle(0, 0, 30);
+            this.addChild();
+            var mc = new createjs.MovieClip(null, 0, true, { start: 20 });
+            this.addChild(mc);
+
+            var child1 = new createjs.Shape(
+                new createjs.Graphics().beginFill("#999999")
+                    .drawCircle(30, 30, 30));
+            var child2 = new createjs.Shape(
+                new createjs.Graphics().beginFill("#5a9cfb")
+                    .drawCircle(30, 30, 30));
+
+            mc.timeline.addTween(
+                createjs.Tween.get(child1)
+                    .to({ x: 0 }).to({ x: 60 }, 50).to({ x: 0 }, 50));
+            mc.timeline.addTween(
+                createjs.Tween.get(child2)
+                    .to({ x: 60 }).to({ x: 0 }, 50).to({ x: 60 }, 50));
+
+            mc.gotoAndPlay("start");
+            mc.shadow = new createjs.Shadow("#000000", 5, 5, 10);
         }
 
         private limitmcClickHandler(evt) {
@@ -172,11 +258,14 @@ module game {
             this.kaipai();
         }
         private clearBetsBtnClickHandler(evt) {
-
+            for (var i = 0; i < this.choumaArr.length; i++) {
+                this.removeChild(this.choumaArr[i]);
+            }
+            this.changeState(FAPAI);
         }
         private flyPaiBtnClickHandler(evt) {
             this.kaipai();
-            this.shoupai();
+            
         }
         private reBetsBtnClickHandler(evt) {
 
@@ -186,6 +275,7 @@ module game {
             this.tablemc["opBtnsMc"]["clearBetsBtn"].removeEventListener("click", this.clearBetsBtnClickHandler.bind(this));
             this.tablemc["opBtnsMc"]["flyPaiBtn"].removeEventListener("click", this.flyPaiBtnClickHandler.bind(this));
             this.tablemc["opBtnsMc"]["reBetsBtn"].removeEventListener("click", this.reBetsBtnClickHandler.bind(this));
+            this.removeEventListener("tick", this.ontickHandler.bind(this));
         }
 
 
@@ -193,12 +283,18 @@ module game {
             var opbtn: createjs.MovieClip = this.tablemc["opBtnsMc"];
             switch (state) {
                 case FAPAI:
+                    this.createFapai();
                     opbtn["clearBetsBtn"].visible = false;
                     opbtn["openPaiBtn"].visible = false;
                     opbtn["flyPaiBtn"].visible = true;
-                    opbtn["reBetsBtn"].visible = true;
+                    opbtn["reBetsBtn"].visible = false;
                     break;
                 case YAZHU:
+                    var chouma = this.choumaBounds.getChildAt(0);
+                    if (!chouma) return;
+                    chouma.x = this.stage.mouseX;
+                    chouma.y = this.stage.mouseY;
+                    this.addChild(chouma);
                     opbtn["clearBetsBtn"].visible = true;
                     opbtn["openPaiBtn"].visible = true;
                     opbtn["flyPaiBtn"].visible = false;
@@ -209,6 +305,7 @@ module game {
                     opbtn["openPaiBtn"].visible = false;
                     opbtn["flyPaiBtn"].visible = false;
                     opbtn["reBetsBtn"].visible = false;
+                    this.jiesuan();
                     break;
             }
             
--- createjstest/src/core/view/panel/SkinPanel.ts
+++ createjstest/src/core/view/panel/SkinPanel.ts
@@ -1,6 +1,21 @@
 ﻿module game {
     /*
-    包含下载功能,下载界面所需的内容;通用资源下载,用资源管理器下载并管理;
+    一个板块就是一个界面的 布局,一个布局,在一个模块或是整个游戏中只产生一次;
+
+    包含下载功能,下载界面所需的内容;通用资源下载用资源管理器下载并管理;
+    1.板块的加载,首先加载显示对象的资源,其次加载音频视频和将来用到的资源;
+    这里的资源处理在板块内部;
+    等待板块的所有的资源都加载完后,开始版块的 业务逻辑;
+
+    2.通用资源加载,用资源管理器下载并管理;
+    统一保存在各自资源类型的管理类中;
+
+
+    板块卸载之后,移除所有的侦听,生成的对象;
+    但保留所有的已经加载的资源;
+    已经加载的资源下次加载无需重新加载??同样的方式加载的貌似只需请求和少量下载;
+
+
     */
     export class SkinPanel extends createjs.MovieClip {
         protected content: createjs.MovieClip;
@@ -10,29 +25,41 @@
             super();
             this.ssKey = sskey;
             this.resourceLoader.addEventListener("fileload", this.handleFileLoad.bind(this));
-            this.resourceLoader.addEventListener("complete", this.handleComplete.bind(this));
-            this.addEventListener("removed", this.removeHandler.bind(this));
+            this.resourceLoader.addEventListener("complete", this.handleLoadComplete.bind(this));
+            this.resourceLoader.on("progress",this.progressHandler);
             ss = ss || {};
             ss[this.ssKey] = ssdata;
+        }   
+        //可多次执行的初始化,与clear对应;
+        public init() {
+            this.addEventListener("added", this.addedHandler.bind(this))
+            this.addEventListener("removed", this.removeHandler.bind(this));
         }
         protected handleFileLoad(evt) {
             console.log("加载资源: " + evt.item.type + " :" + evt.item.id + "完毕");
         }
 
+        protected progressHandler(evt) {
 
-        protected handleComplete(evt) {
+        }
+        protected handleLoadComplete(evt) {
             var queue: createjs.LoadQueue = evt.target;
             console.log(queue.type + "所有游戏资源加载完毕");
             //this.addChild(this.content);
 
-            this.resourceLoader.removeEventListener("fileload", this.handleFileLoad.bind(this));
-            this.resourceLoader.removeEventListener("complete", this.handleComplete.bind(this));
+            
         }
-        protected removeHandler(evt) {
+        protected addedHandler(evt) {
+            this.removeEventListener("added", this.addedHandler.bind(this));
+        }
+        private removeHandler(evt) {
             this.removeEventListener("removed", this.removeHandler.bind(this));
-            this.resourceLoader = null;
+            this.resourceLoader.removeEventListener("fileload", this.handleFileLoad.bind(this));
+            this.resourceLoader.removeEventListener("complete", this.handleLoadComplete.bind(this));
+            this.resourceLoader.off("progress", this.progressHandler);
+            //this.resourceLoader = null;
             this.content = null;
-            this.ssKey = "";
+            //this.ssKey = "";
             this.clear();
         }
         protected clear() {
